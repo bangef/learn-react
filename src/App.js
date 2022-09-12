2f72d2792a6481492117e4466035e18b7e86c65b
bangef <ficrihnp@gmail.com>
2022-09-12 13:14:03 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import Home from "./pages/Home";
 
 function App() {
-  return (
-    <div>
-      {/*
-       * Render Halaman Home
-       * Jika ingin diubah, maka ubah di Halaman Home.
-       */}
-      <Home />
-    </div>
-  );
+	return (
+		<div>
+			<Home />
+		</div>
+	);
 }
 
 export default App;
